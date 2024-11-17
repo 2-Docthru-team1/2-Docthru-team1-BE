f6fdcf0167dcc67cde76885c75576540b073e838
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-17 18:00:59 +0900

    ✨ Feat:#28 refresh token 검증

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,4 +1,5 @@
 import hashPassword from '#middlewares/hashPassword.js';
+import { verifyRefreshToken } from '#middlewares/verifyTokens.js';
 import express from 'express';
 import authController from '#containers/auth.container.js';
 
@@ -8,6 +9,8 @@ export const authRouter = express.Router();
 // 메소드의 리턴값이 아니라 메소드 자체를 넘겨주어야 합니다.
 // app.method로 입력하지 않도록 주의해주세요. router.method입니다.
 authRouter.post('/signUp', hashPassword, authController.signUp);
+
+authRouter.post('/refresh', verifyRefreshToken, authController.refreshToken);
 
 // app에서 사용할 수 있도록 export 해주어야 합니다.
 export default authRouter;
