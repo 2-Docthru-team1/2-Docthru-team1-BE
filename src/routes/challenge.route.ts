692458f7aa4ee7a7afcd11e4fc607e3d6fa33cd2
alscksdlek <mseon916@naver.com>
2024-11-18 22:24:54 +0900

    🔥 Remove: 챌린지 라우트 안 에러핸들러 삭제
    챌린지 라우트 안 에러핸들러 삭제

--- src/routes/challenge.route.ts
+++ src/routes/challenge.route.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import challengeController from '#containers/challenge.container.js';
-import { errorCatchHandler } from '../middlewares/errorCatchHandler.js';
 
 export const challengeRouter = express.Router();
 
@@ -8,6 +7,6 @@ export const challengeRouter = express.Router();
 // 메소드의 리턴값이 아니라 메소드 자체를 넘겨주어야 합니다.
 // app.method로 입력하지 않도록 주의해주세요. router.method입니다.
 challengeRouter.route('/');
-challengeRouter.route('/:id').get(errorCatchHandler(challengeController.getChallengeById));
+challengeRouter.route('/:id').get(challengeController.getChallengeById);
 
 export default challengeRouter;
