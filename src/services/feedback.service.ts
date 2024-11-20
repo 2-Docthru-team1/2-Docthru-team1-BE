31fa260c519b905063ac3b69c10a3b2bad7dd065
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-20 14:13:49 +0900

    ♻️ Refactor: 컨트롤러, 서비스 멤버 이름 수정

--- src/services/feedback.service.ts
+++ src/services/feedback.service.ts
@@ -6,21 +6,21 @@ import { NotFound } from '#types/http-error.type.js';
 import MESSAGES from '#utils/constants/messages.js';
 
 export class FeedbackService implements IFeedbackService {
-  constructor(private FeedbackRepository: FeedbackRepository) {}
+  constructor(private feedbackRepository: FeedbackRepository) {}
 
   getFeedbacks = async (options: {
     orderBy: string;
     page: number;
     pageSize: number;
   }): Promise<{ totalCount: number; list: Feedback[] | null }> => {
-    const totalCount = await this.FeedbackRepository.getCount();
-    const Feedbacks = await this.FeedbackRepository.findMany(options);
+    const totalCount = await this.feedbackRepository.getCount();
+    const Feedbacks = await this.feedbackRepository.findMany(options);
 
     return { totalCount, list: Feedbacks };
   };
 
   getFeedbackById = async (id: string): Promise<Feedback | null> => {
-    const Feedback = await this.FeedbackRepository.findById(id);
+    const Feedback = await this.feedbackRepository.findById(id);
     if (!Feedback) {
       throw new NotFound(MESSAGES.NOT_FOUND);
     }
@@ -29,18 +29,18 @@ export class FeedbackService implements IFeedbackService {
   };
 
   createFeedback = async (FeedbackData: CreateFeedbackDTO): Promise<Feedback> => {
-    const Feedback = await this.FeedbackRepository.create(FeedbackData);
+    const Feedback = await this.feedbackRepository.create(FeedbackData);
 
     return Feedback;
   };
 
   updateFeedback = async (id: string, FeedbackData: UpdateFeedbackDTO): Promise<Feedback> => {
-    const Feedback = await this.FeedbackRepository.update(id, FeedbackData);
+    const Feedback = await this.feedbackRepository.update(id, FeedbackData);
     return Feedback;
   };
 
   deleteFeedback = async (id: string): Promise<Feedback> => {
-    const Feedback = await this.FeedbackRepository.delete(id);
+    const Feedback = await this.feedbackRepository.delete(id);
 
     return Feedback;
   };
