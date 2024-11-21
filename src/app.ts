ab3822c383ed111a93ee111a7bac3c42145a2721
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-21 12:49:24 +0900

    ✨ Feat:#109 s3에 업로드하는 url 발급

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { randomUUID } from 'crypto';
 import express from 'express';
 import 'express-async-errors';
 import { port } from '#configs/common.config.js';
@@ -11,6 +12,8 @@ import challengeRouter from '#routes/challenge.route.js';
 import feedbackRouter from '#routes/feedback.route.js';
 import userRouter from '#routes/user.route.js';
 import workRouter from '#routes/work.route.js';
+import { generatePresignedDownloadUrl } from '#utils/S3/generate-presigned-download-url.js';
+import { generatePresignedUploadUrl } from '#utils/S3/generate-presigned-upload-url.js';
 
 const app = express();
 
@@ -30,6 +33,15 @@ app.use('/works', workRouter);
 
 app.get('/hello', (req, res) => {
   res.send('Hello World');
+});
+app.get('/s3-download', async (req, res) => {
+  const url = await generatePresignedDownloadUrl('GoogleBtn.png');
+  res.send({ url });
+});
+app.get('/s3-upload', async (req, res) => {
+  const s3Key = `userId/${randomUUID()}`;
+  const url = await generatePresignedUploadUrl(s3Key, 'text/plain');
+  res.send({ url });
 });
 
 /*********************************************************************************** handler **********************************************************************************************/
