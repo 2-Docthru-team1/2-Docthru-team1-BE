081fb876bbde80c5597e252bcf3eba8152d91b40
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-21 12:52:00 +0900

    📝 Docs:#109 s3 upload 사용법 주석

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,9 @@ app.get('/s3-download', async (req, res) => {
   res.send({ url });
 });
 app.get('/s3-upload', async (req, res) => {
+  // NOTE s3에 업로드 될 경로
   const s3Key = `userId/${randomUUID()}`;
+  // NOTE 업로드 형식은 기본값이 image/jpeg입니다.
   const url = await generatePresignedUploadUrl(s3Key, 'text/plain');
   res.send({ url });
 });
