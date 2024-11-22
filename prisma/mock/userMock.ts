345645fe7f8ccce4f3aba1b8fa97e9b2f4dafcd6
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-22 09:46:18 +0900

    ✨ Feat:#128 유저 목데이터

---
+++ prisma/mock/userMock.ts
@@ -0,0 +1,126 @@
+import { Role } from '@prisma/client';
+
+const USERS = [
+  {
+    id: 'e8394ccc-7359-4c16-ab37-4ed9671146a4',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName1',
+    email: 'test1@test.com',
+    password:
+      '518d68a9d8b3ac24110c04dd84cabbcf782ff6d3a8ac7c971521413ce3309f153be0e0e7815aa5fd33fb879de795ae162af4807adcf88e8bccca30fddde1c445',
+    salt: 'ac64fc2b0bac39742ff3179ea980d68a',
+    role: Role.normal,
+  },
+  {
+    id: 'c19d6446-2c05-48d7-99d2-9c12d2baa868',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName2',
+    email: 'test2@test.com',
+    password:
+      'c50cad60f6aab5403129b94351c1f24dac0174a176162e6ac00a555211ba8fc50b9191426026a61f1ad541fdf883504b30d3320dacf6017adfa82243341cd456',
+    salt: '7a50ecd4e1cc9770b8c12f733c016f12',
+    role: Role.normal,
+  },
+  {
+    id: '0081e090-abfa-4e86-a13d-3cba9539a06c',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName3',
+    email: 'test3@test.com',
+    password:
+      'a54a14f8d4687905216cb922d9211af9fc7a2502df4600418dbeb6bd0df10ea4ef68dd4e10fbb8b37b86ffd2cc4d6c73ad23115bf997caa9e736442a19fc0e06',
+    salt: '3bd5827f750af866efc4e44efbdfe6b8',
+    role: Role.normal,
+  },
+  {
+    id: '5f72c907-e528-4e38-8b4e-c602417ae8af',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName4',
+    email: 'test4@test.com',
+    password:
+      '1528d62013d78a31c2506dae12d34ab906fa53e66786ed97546977bc94d421ba40f7d01791562ac554a8188bc8026fc67db81a09cadb877584c06b82b9f576de',
+    salt: 'f7eabe156c58c556276d6299c9eacc74',
+    role: Role.normal,
+  },
+  {
+    id: '143c6a80-3522-41e9-a2ed-c57cbdb18f75',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName5',
+    email: 'test5@test.com',
+    password:
+      '634d6644b083c7464fc7942fdfd4596449ed3294aefecfed6aa1178583660e3142d8303b445947d4d74f82c43898f41c3dc724b75cdd0f344eaa0909b2dd431d',
+    salt: '474fbd88a1370e183fad77fafcb4d115',
+    role: Role.normal,
+  },
+  {
+    id: 'bb92be3c-8771-4fe1-aa33-dfeef8061ebe',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName6',
+    email: 'test6@test.com',
+    password:
+      '3acd1d7b8bd49bd2b06ee99b7714a4647fa1e81b678b8adab9f9cc73eb6324c8a81de31fc3759c8aea1b39f30d7a71a05b357fd565df51abb95b0e899d728182',
+    salt: '1000e79260124ebf237e3b49390ea654',
+    role: Role.normal,
+  },
+  {
+    id: '590602e9-3796-4287-bca1-de9969139fbc',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName7',
+    email: 'test7@test.com',
+    password:
+      'dafc3a9b8dbe2a734eb397f85d35ad8b7f6a67dbf0de720bb3d383c4bde1a163ac377f62537b26aa578832381601885a4ecd8fcf11eb18831c5f571dc6725260',
+    salt: '036866550ba07982d46e1acf102d72da',
+    role: Role.normal,
+  },
+  {
+    id: 'a37fec0c-d754-44d4-b758-1c9d5c76774f',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName8',
+    email: 'test8@test.com',
+    password:
+      '8efbd5cf529d3a4068ef1246b473935eb008f57dd05b8d690b716d215b1ef30227d9f32d146867d35ce4e1b62a3cbb6eebd756a279b67fd9f9a3775391034c0e',
+    salt: '355b29762f3c480bf7c435d5cbb1be43',
+    role: Role.admin,
+  },
+  {
+    id: '78dd77e8-36e4-4cbd-bd7b-27e7fcaae6f9',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName9',
+    email: 'test9@test.com',
+    password:
+      '798b6b61242de24793c38e361ea048e8342c6cd6cd1704f1da521eedba780f84adb8883f17f11ffc246f7bcccad1c4c4ee46a80f88de07bfa503005a9bd490a7',
+    salt: '8a63467c914bb43b923f2deb21ac25d0',
+    role: Role.admin,
+  },
+  {
+    id: 'a666a76c-46a1-4aa2-9741-bdbb20785fc4',
+    createdAt: new Date('2024-11-16T07:11:43.704559'),
+    updatedAt: new Date('2024-11-16T07:11:43.704559'),
+    deleteAt: null,
+    name: 'testNickName10',
+    email: 'test10@test.com',
+    password:
+      'd0c223867ff1fee20b854100d26cb916127ab6416092d234a212efb59d7f6aa42ded8dfaac2351e41841f987b01a3d4653aeb556a13196814251ca5c279a0185',
+    salt: 'b97cb0c8eda438e34f849a7059f60b37',
+    role: Role.admin,
+  },
+];
+
+export default USERS;
