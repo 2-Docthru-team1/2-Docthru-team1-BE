b00035233130ac647c88d186d1e900b7518dd57d
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-24 00:19:06 +0900

    🐛 Fix: 레시피 dto 문제 해결

--- src/repositories/work.repository.ts
+++ src/repositories/work.repository.ts
@@ -1,10 +1,7 @@
 import type { ChallengeWork } from '@prisma/client';
-import type { ChallengeWork, PrismaClient } from '@prisma/client';
-import { randomUUID } from 'crypto';
 import type { IWorkRepository } from '#interfaces/repositories/work.repository.interface.js';
 import type { ExtendedPrismaClient } from '#types/common.types.js';
 import { type CreateWorkDTO, type GetWorksOptions, type UpdateWorkDTO, WorkOrder } from '#types/work.types.js';
-import { generatePresignedUploadUrl } from '#utils/S3/generate-presigned-upload-url.js';
 
 export class WorkRepository implements IWorkRepository {
   constructor(private challengeWork: ExtendedPrismaClient['challengeWork']) {}
