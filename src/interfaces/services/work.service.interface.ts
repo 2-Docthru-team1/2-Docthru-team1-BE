ada53f06aec1ad2d8efb8eab8e44ff74ecc835cd
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-24 17:25:15 +0900

    241124:1725

--- src/interfaces/services/work.service.interface.ts
+++ src/interfaces/services/work.service.interface.ts
@@ -1,10 +1,10 @@
 import type { ChallengeWork } from '@prisma/client';
-import type { CreateWorkDTO, GetWorksOptions, ResultChallengeWork, UpdateWorkDTO, WorkOrder } from '#types/work.types.js';
+import type { CreateWorkDTO, GetWorksOptions, ResultChallengeWork, UpdateWorkDTO, WorkResponse } from '#types/work.types.js';
 
 export interface IWorkService {
   getWorks(options: GetWorksOptions): Promise<{ list: ResultChallengeWork[]; totalCount: number } | null>;
   getWorkById(id: string): Promise<ResultChallengeWork | null>;
   createWork(WorkData: CreateWorkDTO): Promise<ChallengeWork>;
-  updateWork(id: string, WorkData: UpdateWorkDTO): Promise<ChallengeWork>;
+  updateWork(id: string, WorkData: UpdateWorkDTO): Promise<WorkResponse>;
   deleteWork(id: string): Promise<ResultChallengeWork>;
 }
