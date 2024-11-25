784989fa544fc75d2b4ee75009cfe4352dc2737f
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-25 11:47:56 +0900

    add debug paths

--- src/routes/work.route.ts
+++ src/routes/work.route.ts
@@ -10,12 +10,12 @@ export const workRouter = express.Router();
 // app.method로 입력하지 않도록 주의해주세요. router.method입니다.
 //workRouter.route('/').get;
 workRouter.route('/').get(workController.getWorks);
-workRouter.route('/:id/feedbacks').get(feedbackController.getFeedbacks);
 workRouter
   .route('/:id')
   .get(workController.getWorkById)
   .patch(tokenVerifier.verifyAccessToken, workController.patchWork)
   .delete(tokenVerifier.verifyAccessToken, workController.deleteWork);
 
+workRouter.route('/:id/feedbacks').get(feedbackController.getFeedbacks);
 // app에서 사용할 수 있도록 export 해주어야 합니다
 export default workRouter;
