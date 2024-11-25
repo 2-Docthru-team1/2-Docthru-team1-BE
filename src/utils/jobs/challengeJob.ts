75a4111f4b7ad597042efeb6ba9e7fb5052352bd
alscksdlek <mseon916@naver.com>
2024-11-25 15:22:40 +0900

    ✨ Feat: 자정에만 상태 체크로 변경

--- src/utils/jobs/challengeJob.ts
+++ src/utils/jobs/challengeJob.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import prismaClient from '#connection/postgres.connection.js';
 
 export const scheduleChallengeStatus = () => {
-  cron.schedule('* * * * *', async () => {
+  cron.schedule('0 0 * * *', async () => {
     console.log('test');
     const now = new Date();
     const updateChallengeStatusFinished = await prismaClient.challenge.updateMany({
