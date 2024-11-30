b2535edf0cc761f02b21ee815e379c612a38cf93
TaeyeonKim <gggm0117@naver.com>
2024-11-30 14:00:03 +0900

    🐛 Fix #209 거절,취소 사유 목데이터 추가
    목데이터안의 거절, 취소 상태의 챌린지가 있지만 그에 해당하는 거절,취소 사유 목데이터가 없으므로 추가

---
+++ prisma/mock/abortReasonMock.ts
@@ -0,0 +1,22 @@
+export const ABORT_REASONS = [
+  {
+    content: '거절,취소 사유',
+    adminId: '590602e9-3796-4287-bca1-de9969139fbc',
+    challengeId: 'd0611542-61b2-4ff1-8e3d-745413cdfce5',
+  },
+  {
+    content: '거절,취소 사유',
+    adminId: '590602e9-3796-4287-bca1-de9969139fbc',
+    challengeId: '3508f13c-7399-48d1-b35a-a9e5e2ed4b06',
+  },
+  {
+    content: '거절,취소 사유',
+    adminId: '590602e9-3796-4287-bca1-de9969139fbc',
+    challengeId: 'c756848e-8940-41bd-959f-68e0f34563fc',
+  },
+  {
+    content: '거절,취소 사유',
+    adminId: '590602e9-3796-4287-bca1-de9969139fbc',
+    challengeId: 'fc299a7d-9b3f-4ac6-afa5-17d035bc3388',
+  },
+];
