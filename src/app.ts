476b8c7d90c861e3b5fda12c47a834718e81405f
csbizz <95736373+csbizz@users.noreply.github.com>
2024-11-30 18:46:15 +0900

    Origin to localhost:3000

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ startJob();
 
 /*********************************************************************************** middlewares **********************************************************************************************/
 app.use(express.json());
-app.use(cors({ credentials: true, origin: '*' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(validatePaginationOptions);
 app.use(runAsyncLocalStorage);
