6cffa55eb9e9dc9fcd74f3afcb0f3bf3904f3f0f
csbizz <95736373+csbizz@users.noreply.github.com>
2024-12-03 10:43:25 +0900

    🐛 Fix:#238 feedback 목록 조회시 상위 work 확인

--- src/containers/feedback.container.ts
+++ src/containers/feedback.container.ts
@@ -1,10 +1,12 @@
 import prismaClient from '#connection/postgres.connection.js';
 import { FeedbackController } from '#controllers/feedback.controller.js';
 import { FeedbackRepository } from '#repositories/feedback.repository.js';
+import { WorkRepository } from '#repositories/work.repository.js';
 import { FeedbackService } from '#services/feedback.service.js';
 
 const feedbackRepository = new FeedbackRepository(prismaClient);
-const feedbackService = new FeedbackService(feedbackRepository);
+const workRepository = new WorkRepository(prismaClient);
+const feedbackService = new FeedbackService(feedbackRepository, workRepository);
 const feedbackController = new FeedbackController(feedbackService);
 
 export default feedbackController;
