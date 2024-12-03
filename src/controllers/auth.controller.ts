6e49e9572a984bdbe032da7fccd3fd9437b195f1
csbizz <95736373+csbizz@users.noreply.github.com>
2024-12-03 14:03:27 +0900

    🐛 Fix: cookie to none/false

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -23,7 +23,7 @@ export class AuthController {
     // });
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: false,
     });
 
