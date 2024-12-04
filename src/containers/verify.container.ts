27741e43d5e211e6bb189f9d7a933c407e0b73b9
csbizz <95736373+csbizz@users.noreply.github.com>
2024-12-04 18:59:51 +0900

    🐛 Fix: 핫픽스 241204

--- src/containers/verify.container.ts
+++ src/containers/verify.container.ts
@@ -1,10 +1,12 @@
 import prismaClient from '#connection/postgres.connection.js';
 import { TokenVerifier } from '#middlewares/token.verifier.js';
 import { UserRepository } from '#repositories/user.repository.js';
+import { WorkLikeRepository } from '#repositories/workLike.repository.js';
 import { UserService } from '#services/user.service.js';
 
 const userRepository = new UserRepository(prismaClient);
-const userService = new UserService(userRepository);
+const workLikeRepository = new WorkLikeRepository(prismaClient);
+const userService = new UserService(userRepository, workLikeRepository);
 const tokenVerifier = new TokenVerifier(userService);
 
 export default tokenVerifier;
