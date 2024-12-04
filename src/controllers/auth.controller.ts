729e3734e0f0ebc930fe9641a20b55987452871b
csbizz <95736373+csbizz@users.noreply.github.com>
2024-12-04 14:08:52 +0900

    🐛 Fix: refresh 방식 변경

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,11 @@
+import type { NextFunction, Response } from 'express';
+import { assert } from 'superstruct';
 import type { AuthService } from '#services/auth.service.js';
 import type { CreateUserDTO, SignInDTO } from '#types/auth.types.js';
 import type { Request } from '#types/common.types.js';
-import { BadRequest, NotFound, Unauthorized } from '#types/http-error.types.js';
 import MESSAGES from '#utils/constants/messages.js';
 import sendVerificationMail from '#utils/nodemailer/verification-message.js';
 import { CreateUser, SignIn, Uuid } from '#utils/struct.js';
-import type { NextFunction, Response } from 'express';
-import { assert } from 'superstruct';
 
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -44,22 +43,26 @@ export class AuthController {
     res.json(user);
   };
 
-  refreshToken = async (req: Request<{body: {refreshToken: string}}>, res: Response, next: NextFunction) => {
-    // const { refreshToken } = req.cookies;
-    const { refreshToken } = req.body;
-
-    if (refreshToken === undefined) {
-      throw new BadRequest(MESSAGES.NO_REFRESH_TOKEN);
-    }
-    if (!req.user || !req.user.userId) {
-      throw new Unauthorized(MESSAGES.UNAUTHORIZED);
-    }
-
-    const user = await this.authService.getNewToken(req.user, refreshToken);
-    if (!user) {
-      throw new NotFound(MESSAGES.NOT_FOUND);
-    }
+  refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await this.authService.getNewToken();
 
     res.json(user);
   };
+  // refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  //   const { refreshToken } = req.cookies;
+
+  //   if (refreshToken === undefined) {
+  //     throw new BadRequest(MESSAGES.NO_REFRESH_TOKEN);
+  //   }
+  //   if (!req.user || !req.user.userId) {
+  //     throw new Unauthorized(MESSAGES.UNAUTHORIZED);
+  //   }
+
+  //   const user = await this.authService.getNewToken(req.user, refreshToken);
+  //   if (!user) {
+  //     throw new NotFound(MESSAGES.NOT_FOUND);
+  //   }
+
+  //   res.json(user);
+  // };
 }
