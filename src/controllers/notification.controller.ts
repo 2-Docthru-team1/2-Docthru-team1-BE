3c8887898c0ca483a3cea1c4c2630221a12f292d
alscksdlek <mseon916@naver.com>
2024-12-05 16:12:57 +0900

    ✨ Feat: uuid 유효성 검사 추가

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -5,6 +5,7 @@ import type { Request } from '#types/common.types.js';
 import type { UpdateNotificationDTO } from '#types/notification.types.js';
 import MESSAGES from '#utils/constants/messages.js';
 import { PatchNotification } from '#utils/struct.js';
+import { Uuid } from '#utils/struct.js';
 
 export class NotificationController {
   constructor(private notificationService: NotificationService) {}
@@ -17,6 +18,7 @@ export class NotificationController {
   patchNotification = async (req: Request<{ params: { id: string }; body: UpdateNotificationDTO }>, res: Response) => {
     assert(req.body, PatchNotification, MESSAGES.WRONG_FORMAT);
     const { id } = req.params;
+    assert(id, Uuid, MESSAGES.WRONG_ID_FORMAT);
 
     const updateNotification = await this.notificationService.updateNotification(id, req.body);
     res.json(updateNotification);
